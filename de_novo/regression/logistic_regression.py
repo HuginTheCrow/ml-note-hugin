b6682b8d094790df38d73382b3f9abb2a0ee81cb
HuginTheCrow <huginthecrow@proton.me>
2023-05-15 22:59:32 +0000

    set up project

--- de_novo/regression/logistic_regression.py
+++ de_novo/regression/logistic_regression.py
@@ -8,4 +8,15 @@ import numpy as np
 import os
 
 path = r'..\data'
-training_sample =
+training_sample = 'Logistic_Regression-trainingSample.txt'
+testing_sample = 'Logistic_Regression-testingSample.txt'
+
+# 从文件中读入训练样本的数据
+def loadDataSet(p, file_n):
+    dataMat = []
+    labelMat = []
+    fr = open(os.path.join(p, file_n))
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        # 三个特征x0, x1, x2, x0=1
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
