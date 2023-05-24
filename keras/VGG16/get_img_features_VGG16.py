b4e51abb98a313f4baf7a2abf366d508b4900ab8
HuginTheCrow <huginthecrow@proton.me>
2023-05-24 16:59:57 +0000

    bump version to 0.2.4

--- keras/VGG16/get_img_features_VGG16.py
+++ keras/VGG16/get_img_features_VGG16.py
@@ -18,4 +18,12 @@ def vgg16_model(weights_path):
     model.add(Conv2D(64, (3, 3), activation='relu', padding='same', 
                      input_shape=(224, 224, 3), name='block1_conv1'))
     model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2), name
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+
+    # Block 2
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1'))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+
+    # Block 3
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same
