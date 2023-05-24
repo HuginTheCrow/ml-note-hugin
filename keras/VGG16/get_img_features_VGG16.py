3a006c68f116be0d918736293df98438c8b1a60a
HuginTheCrow <huginthecrow@proton.me>
2023-05-24 13:56:15 +0000

    fixed #26

--- keras/VGG16/get_img_features_VGG16.py
+++ keras/VGG16/get_img_features_VGG16.py
@@ -1,1 +1,14 @@
-# https://keras-cn.readthedocs.io/en/l
+# https://keras-cn.readthedocs.io/en/latest/other/application/
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import (Flatten, Dense, Conv2D, MaxPooling2D)
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+import numpy as np
+import os
+import argparse
+import json
+
+
+def vgg16_model(weights_path):
+    # this modle totaly has 22 layers with polling 
