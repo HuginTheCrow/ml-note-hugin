2cd3275804a992e262ea4601eb14f2a7ae8b12fc
HuginTheCrow <huginthecrow@proton.me>
2023-06-11 08:20:43 +0000

    adding references

---
+++ probability_basic/LLN_and_CLT/CLT_new.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+"""
+Created on Sun Nov 17 18:44:37 2017
+
+@author: Belter
+"""
+
+
+def sampling2pmf(n, dist, t=100000):
+    """
+    n: sample size for each experiment
