de9c365f27e5a184c2a064cc4fa2b793cd885636
HuginTheCrow <huginthecrow@proton.me>
2023-06-11 19:07:53 +0000

    remove extra blank line

--- probability_basic/continuous_distributions/compare_poission_exp.py
+++ probability_basic/continuous_distributions/compare_poission_exp.py
@@ -5,4 +5,13 @@ import matplotlib.pyplot as plt
 
 def compare_poission_exp():
     """
-    This post explained the r
+    This post explained the relation between these two distribution
+      - https://stats.stackexchange.com/a/2094/134555
+      - P(Xt <= x) = 1 - e^(-lambda * x)
+    Now, I suppose lambda=1, just like this example(from wiki, Poisson_distribution):
+      - On a particular river, overflow floods occur once every 100 years on average.
+    :return:
+    """
+    x = np.arange(20)
+    y1 = 1 - np.power(np.e, -x)  # lambda = 1
+    y2 = 1 - np.
