a8935dd2553e6f98ca3093a093fa2a8da58511c7
HuginTheCrow <huginthecrow@proton.me>
2023-06-11 15:38:04 +0000

    Update versions in README

--- probability_basic/LLN_and_CLT/CLT_new.py
+++ probability_basic/LLN_and_CLT/CLT_new.py
@@ -37,4 +37,13 @@ def plot(n, dist, subplot):
     sigma = np.sqrt(n * dist.var())
     samples = sampling2pmf(n=n, dist=dist)
     # normed参数可以对直方图进行标准化，从而使纵坐标表示概率而不是次数
-    plt.hist(samples, normed=True, bins=100, 
+    plt.hist(samples, normed=True, bins=100, color='#348ABD',
+             label='{} RVs'.format(n))
+    plt.ylabel('Probability')
+    # normal distribution
+    norm_dis = stats.norm(mu, sigma)
+    norm_x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 10000)
+    pdf = norm_dis.pdf(norm_x)
+    plt.plot(norm_x, pdf, 'r--', alpha=0.6, label='N(${0:.0f}, {1:.2f}^2$)'.format(mu, sigma))
+    plt.legend(loc='upper left', prop={'size': 8})
+
