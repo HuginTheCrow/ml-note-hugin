4739d86660402b7826865e61209ad4682f0ecd68
HuginTheCrow <huginthecrow@proton.me>
2023-06-13 22:45:48 +0000

    git push

--- probability_basic/continuous_distributions/continuous_distributions.py
+++ probability_basic/continuous_distributions/continuous_distributions.py
@@ -116,4 +116,18 @@ def normal_dis(miu=0, sigma=1):
     # 计算该分布在x中个点的概率密度分布函数值(PDF)
     pdf = norm_dis.pdf(x)
 
-    # 计算该分布在x中个点的累计分布函数
+    # 计算该分布在x中个点的累计分布函数值(CDF)
+    cdf = norm_dis.cdf(x)
+
+    # 下面是利用matplotlib画图
+    plt.figure(1)
+    # plot pdf
+    plt.subplot(211)  # 两行一列，第一个子图
+    plt.plot(x, pdf, 'b-',  label='pdf')
+    plt.ylabel('Probability')
+    plt.title(r'PDF/CDF of normal distribution')
+    plt.text(-5.0, .12, r'$\mu={},\ \sigma={}$'.format(miu, sigma))  # 3是标准差，不是方差
+    plt.legend(loc='best', frameon=False)
+    # plot cdf
+    plt.subplot(212)
+    p
