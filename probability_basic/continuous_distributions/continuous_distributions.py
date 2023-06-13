91a669127612d8a992ee34955bde4a902ec59601
HuginTheCrow <huginthecrow@proton.me>
2023-06-13 06:58:11 +0000

    fb

--- probability_basic/continuous_distributions/continuous_distributions.py
+++ probability_basic/continuous_distributions/continuous_distributions.py
@@ -11,4 +11,17 @@ def uniform_distribution(loc=0, scale=1):
     :return:
     """
     uniform_dis = stats.uniform(loc=loc, scale=scale)
-    x = np.linspace(uniform_
+    x = np.linspace(uniform_dis.ppf(0.01),
+                    uniform_dis.ppf(0.99), 100)
+    fig, ax = plt.subplots(1, 1)
+
+    # 直接传入参数
+    ax.plot(x, stats.uniform.pdf(x, loc=2, scale=4), 'r-',
+            lw=5, alpha=0.6, label='uniform pdf')
+
+    # 从冻结的均匀分布取值
+    ax.plot(x, uniform_dis.pdf(x), 'k-',
+            lw=2, label='frozen pdf')
+
+    # 计算ppf分别等于0.001, 0.5, 0.999时的x值
+    vals = uniform_dis.ppf([0.001, 
