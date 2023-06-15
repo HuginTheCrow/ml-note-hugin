93f248f6276c24616b5d17b46eca364f55106ce2
HuginTheCrow <huginthecrow@proton.me>
2023-06-15 11:51:23 +0000

    Update guildwars.js

--- probability_basic/discrete_distributions/use_a_distribution.py
+++ probability_basic/discrete_distributions/use_a_distribution.py
@@ -13,4 +13,17 @@ pdf = norm_dis.pdf(x)
 # 计算该分布在x中个点的累计分布函数值(CDF)
 cdf = norm_dis.cdf(x)
 
-# 下面是利用matplotlib
+# 下面是利用matplotlib画图
+plt.figure(1)
+# plot pdf
+plt.subplot(211)  # 两行一列，第一个子图
+plt.plot(x, pdf, 'b-',  label='pdf')
+plt.ylabel('Probability')
+plt.title(r'PDF/CDF of normal distribution')
+plt.text(-5.0, .12, r'$\mu=5,\ \sigma=3$')  # 3是标准差，不是方差
+plt.legend(loc='best', frameon=False)
+# plot cdf
+plt.subplot(212)
+plt.plot(x, cdf, 'r-', label='cdf')
+plt.ylabel('Probability')
+plt.legend(loc='best', fram
