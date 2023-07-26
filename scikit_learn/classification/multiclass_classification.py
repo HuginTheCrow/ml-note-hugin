28bb7ee5a3cc8a26f149e36b092e8541879598cc
HuginTheCrow <huginthecrow@proton.me>
2023-07-26 21:02:54 +0000

    Despliegue DB en Heroku

--- scikit_learn/classification/multiclass_classification.py
+++ scikit_learn/classification/multiclass_classification.py
@@ -7,4 +7,14 @@ from sklearn.datasets import fetch_mldata
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import SGDClassifier  # Stochastic Gradient Des
+from sklearn.linear_model import SGDClassifier  # Stochastic Gradient Descent
+from sklearn.model_selection import cross_val_score  # 用于训练集内部交叉验证
+from sklearn.model_selection import cross_val_predict
+from sklearn.preprocessing import StandardScaler  # 用于输入数据的预处理
+from sklearn.metrics import confusion_matrix  # 用于评价模型
+
+
+# MINIST dataset
+# mnist = fetch_mldata('MNIST original')
+custom_data_home = r'D:\github\datasets'
+# URL, http://mlda
