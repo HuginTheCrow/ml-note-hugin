8f84da9e19742b3b64bc2a9ee42a6894625e496c
HuginTheCrow <huginthecrow@proton.me>
2023-08-02 19:41:58 +0000

    DP

--- tensor_flow/DB-VAE/db_vae.py
+++ tensor_flow/DB-VAE/db_vae.py
@@ -1,3 +1,17 @@
 """
 MIT 6.S191, lab2
-https://github.com/aamini/introto
+https://github.com/aamini/introtodeeplearning/blob/master/lab2/Part2_Debiasing.ipynb
+http://introtodeeplearning.com/AAAI_MitigatingAlgorithmicBias.pdf
+"""
+
+import IPython
+import tensorflow as tf
+import functools
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+import mitdeeplearning as mdl
+
+
+def make_standard_classifier(n_outputs=1, n_filters=12):
+    """Function to define a standard CNN m
