9e8c93140a8cbe8ef975ab1bf6463b143eb972e5
HuginTheCrow <huginthecrow@proton.me>
2023-08-19 21:25:46 +0000

    Updating LICENSE

--- tensor_flow/VGG16/vgg16_tf.py
+++ tensor_flow/VGG16/vgg16_tf.py
@@ -1,4 +1,9 @@
 ########################################################################################
 # Davi Frossard, 2016                                                                  #
 # VGG16 implementation in TensorFlow                                                   #
-# Details:                                  
+# Details:                                                                             #
+# http://www.cs.toronto.edu/~frossard/post/vgg16/                                      #
+#                                                                                      #
+# Model from https://gist.github.com/ksimonyan/211839e770f7b538e2d8#file-readme-md     #
+# Weights from Caffe converted using https://github.com/ethereon/caffe-tensorflow      #
+###
