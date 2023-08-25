854f35f3f8689d1afe406fea68fb07a9eecca50a
HuginTheCrow <huginthecrow@proton.me>
2023-08-25 09:42:57 +0000

    Update presentation

--- tensor_flow/VGG16/vgg16_tf.py
+++ tensor_flow/VGG16/vgg16_tf.py
@@ -137,3 +137,11 @@ class vgg16:
             conv = tf.nn.conv2d(self.pool3, kernel, [1, 1, 1, 1], padding='SAME')
             biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
                                  trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv4_1 = tf.nn.relu(out, name=scope)
+            self.parameters += [kernel, biases]
+
+        # conv4_2
+        with tf.name_scope('conv4_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3, 3, 512, 512], dtype=tf.float32,
+                                                     stddev=1e-1),
