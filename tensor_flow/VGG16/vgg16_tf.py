a99b38aaae81de748561a0e0fc6130b05e1eb365
HuginTheCrow <huginthecrow@proton.me>
2023-08-27 09:15:01 +0000

    changed name

--- tensor_flow/VGG16/vgg16_tf.py
+++ tensor_flow/VGG16/vgg16_tf.py
@@ -186,4 +186,12 @@ class vgg16:
             kernel = tf.Variable(tf.truncated_normal([3, 3, 512, 512], dtype=tf.float32,
                                                      stddev=1e-1), name='weights')
             conv = tf.nn.conv2d(self.conv5_1, kernel, [1, 1, 1, 1], padding='SAME')
-            biases = tf.Variable(tf.constant(0.0,
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
+                                 trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv5_2 = tf.nn.relu(out, name=scope)
+            self.parameters += [kernel, biases]
+
+        # conv5_3
+        with tf.name_scope('conv5_3') as scope:
+            kernel = tf.Variable(tf.truncated_n
