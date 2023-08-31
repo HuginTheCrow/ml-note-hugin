1db54cf847eb01681acb2aa742aa8caf53327897
HuginTheCrow <huginthecrow@proton.me>
2023-08-31 14:13:36 +0000

    fixes #66

--- tensor_flow/VGG16/vgg16_tf.py
+++ tensor_flow/VGG16/vgg16_tf.py
@@ -260,4 +260,7 @@ if __name__ == '__main__':
     img1 = imread('laska.png', mode='RGB')
     img1 = imresize(img1, (224, 224))
 
-    prob = sess.run(vgg.probs, feed_dict={vgg.imgs: [i
+    prob = sess.run(vgg.probs, feed_dict={vgg.imgs: [img1]})[0]
+    preds = (np.argsort(prob)[::-1])[0:5]
+    for p in preds:
+        print(class_names[p], prob[p])
---
+++ tensor_flow/regression/linear_regression_example1.py
@@ -0,0 +1,11 @@
+# this example comes from standford's cs224d lecture7, tensorflow tutorial
+# https://cs224d.stanford.edu/lectures/
+import numpy as np
+# import seaborn
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+# define input data
+X_data = np.arange(100, step=.1)
+y_da
