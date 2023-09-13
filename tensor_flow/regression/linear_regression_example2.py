d6d4a593b1296a0843f43a2179f846a2acbd078c
HuginTheCrow <huginthecrow@proton.me>
2023-09-13 21:42:59 +0000

    1.12.3

--- tensor_flow/regression/linear_regression_example2.py
+++ tensor_flow/regression/linear_regression_example2.py
@@ -150,4 +150,14 @@ def train_theta_by_mini_batch_gd():
     n_batches = int(np.ceil(m / batch_size))
 
     def fetch_batch(epoch, batch_index, batch_size):
-        np.random.seed(epoch * n_batches + batch_index)  # 每次生成不同
+        np.random.seed(epoch * n_batches + batch_index)  # 每次生成不同的seed
+        indices = np.random.randint(m, size=batch_size)  # 随机生成batch_size个小于m的整数
+        X_batch = scaled_housing_data_plus_bias[indices]
+        y_batch = housing.target.reshape(-1, 1)[indices]
+        return X_batch, y_batch
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(n_epochs):
+            loss = 0
+            for batch_index in range(n_batc
