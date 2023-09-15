e2b8c85c6557ad9ff929cc76b816af3765efd889
HuginTheCrow <huginthecrow@proton.me>
2023-09-15 14:40:54 +0000

    change path

--- tensor_flow/save_and_restore/save_reuse_parameters.py
+++ tensor_flow/save_and_restore/save_reuse_parameters.py
@@ -3,4 +3,14 @@
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 import tensorflow as tf
-from sklearn.preprocessing impo
+from sklearn.preprocessing import StandardScaler  #  用于数据缩放
+
+housing = fetch_california_housing()
+m, n = housing.data.shape  # m是样本数，n是特征的数量
+print(m, n)
+
+# Gradient Descent requires scaling the feature vectors first
+# X的缩放对后面的训练过程影响非常大，经过缩放的数据经过很少的迭代次数就可以收敛，学习率可以设得很大
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housi
